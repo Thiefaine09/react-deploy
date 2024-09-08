8e0d9de12492a4a792682d481deebffdba9c37c0
Thiefaine09 <thiefaine.ines@gmail.com>
2024-09-08 23:24:19 +0200

    Mise à jour du site avec les nouvelles modifications

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,7 +44,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="/"
+                href="https://www.instagram.com/thiefainehrq_/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,8 +25,8 @@ function Projects() {
               isBlog={false}
               title="BistroK Restaurant Lounge"
               description="Le BistroK est un restaurant lounge réalisé en React et Next.js, avec une conception utilisant HTML et CSS. Il offre une expérience gastronomique unique, avec des fonctionnalités telles que la réservation en ligne, la consultation du menu et la découverte de l'ambiance côté Lounge."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/Thiefaine09"
+              demoLink="https://github.com/Thiefaine09"
             />
           </Col>
 
@@ -36,8 +36,8 @@ function Projects() {
               isBlog={false}
               title="Portfolio Therique"
               description="Therique est un portfolio complet réalisé exclusivement avec Bootstrap, HTML, CSS et JavaScript. Il met en valeur mes compétences, mes projets et mon parcours professionnel de manière attrayante et professionnelle."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/Thiefaine09"
+              demoLink="https://github.com/Thiefaine09"
             />
           </Col>
 
@@ -47,8 +47,8 @@ function Projects() {
               isBlog={false}
               title="CodeCraft Innovations"
               description="Le site vitrine de CodeCraft Innovations est une création soignée et professionnelle qui met en avant une entreprise spécialisée dans le développement informatique. Conçu avec une attention particulière aux détails, ce site a été développé pour refléter les valeurs et les compétences de l'entreprise, tout en offrant une expérience utilisateur intuitive."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              ghLink="https://github.com/Thiefaine09"
+              demoLink="https://github.com/Thiefaine09"
             />
           </Col>
 
@@ -58,8 +58,8 @@ function Projects() {
               isBlog={false}
               title="Gestion Client Coiffure"
               description="Est une application innovante développée en C# qui permet aux coiffeurs de gérer efficacement leurs rendez-vous et leurs clients en temps réel. Cette application offre une gamme complète de fonctionnalités pour améliorer la productivité et l'organisation des salons de coiffure."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="https://github.com/Thiefaine09"
+              demoLink="https://github.com/Thiefaine09"
             />
           </Col>
         </Row>
