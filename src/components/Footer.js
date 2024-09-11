2c04b5a20dd626219cf091eabcaa5c42693ffc00
Thiefaine09 <thiefaine.ines@gmail.com>
2024-09-11 18:53:09 +0200

    Mise à jour du site avec les nouvelles modifications

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Etudiant Bachelor Consultant Développeur Web et Application Mobile</h3>
+          <h3>Consultant Développeur Web et Entrepeneur</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Code-Thief</h3>
