bf5bc9bbffbc083ace314430a813abf071034332
Thiefaine Herique <114389734+Thiefaine09@users.noreply.github.com>
2025-06-26 01:26:36 +0200

    Mise à jour du composant ResumeNew

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV-ThiefaineH-MAJ.pdf";
+import pdf from "../../Assets/../Assets/CV_OFF_2025Thief.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
