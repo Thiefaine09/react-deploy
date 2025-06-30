b4330a43b7e324848453c454f587fde1d8a2210d
Thiefaine Herique <114389734+Thiefaine09@users.noreply.github.com>
2025-06-30 17:22:07 +0200

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
+import pdf from "../../Assets/CV_OFFThiefaine.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-
-// ✅ Lien brut vers le PDF sur GitHub
-const pdf = "https://raw.githubusercontent.com/thiefaine09/react-deploy/main/src/Assets/CV_OFFThiefaine.pdf";
-
-// ✅ Configuration de PDF.js
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
@@ -23,7 +19,8 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-
+        
+    
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
